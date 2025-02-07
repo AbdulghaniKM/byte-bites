bebf82e39ad0f4473f62bb98c0e39eed394707ac
AbdulghaniKM <abdulghanim70@gmail.com>
2025-02-07 23:32:13 +0300

    fix: :bug: tabs scroll on sm

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -18,15 +18,41 @@
       Welcome to Byte-Bites, your go-to destination for delicious recipes!
     </h1>
     <!-- Category Tabs -->
-    <div class="flex mb-8 space-x-4">
-      <Tab
-        v-for="category in filteredCategories"
-        :key="category"
-        :is-active="activeCategory === category.strCategory"
-        @click="selectCategory(category.strCategory)"
+    <div class="relative w-full max-w-full mb-8">
+      <div
+        ref="tabsContainer"
+        class="flex overflow-x-auto scrollbar-hide"
+        style="scroll-behavior: smooth"
       >
-        {{ category.strCategory }}
-      </Tab>
+        <div class="flex w-full px-4 space-x-4">
+          <Tab
+            v-for="category in filteredCategories"
+            :key="category"
+            :is-active="activeCategory === category.strCategory"
+            @click="selectCategory(category.strCategory)"
+            class="flex-shrink-0"
+          >
+            {{ category.strCategory }}
+          </Tab>
+        </div>
+      </div>
+      <!-- Refined Gradient Shadows -->
+      <div
+        class="absolute inset-y-0 left-0 w-16 pointer-events-none bg-gradient-to-r opacity-90"
+        :class="[
+          `from-${themeStore.getThemeColor('100')}`,
+          `dark:from-${themeStore.getThemeColor('950')}`,
+          'via-transparent to-transparent',
+        ]"
+      ></div>
+      <div
+        class="absolute inset-y-0 right-0 w-16 pointer-events-none bg-gradient-to-l opacity-90"
+        :class="[
+          `from-${themeStore.getThemeColor('100')}`,
+          `dark:from-${themeStore.getThemeColor('950')}`,
+          'via-transparent to-transparent',
+        ]"
+      ></div>
     </div>
 
     <!-- Loading State -->
@@ -104,3 +130,14 @@
     categories.value = await mealsCat.fetchCategory();
   });
 </script>
+
+<style scoped>
+  .scrollbar-hide {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+
+  .scrollbar-hide::-webkit-scrollbar {
+    display: none; /* Chrome, Safari and Opera */
+  }
+</style>
